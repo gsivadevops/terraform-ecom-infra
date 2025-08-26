fe8d0de9d38f4b507df27866669b36b2170c9884
sivakumar <gsivadevops@gmail.com>
2025-08-26 07:09:20 +0530

    route53 record for vpn

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -21,3 +21,13 @@ resource "aws_instance" "vpn" {
     }
   )
 }
+
+# route53 record for vpn
+resource "aws_route53_record" "vpn" {
+  zone_id = var.zone_id
+  name    = "vpn-${var.environment}.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.vpn.public_ip]
+  allow_overwrite = true
+}
