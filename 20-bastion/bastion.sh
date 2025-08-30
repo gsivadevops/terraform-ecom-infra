f6197147b508b38e728aac969a78e5ee08909790
sivakumar <gsivadevops@gmail.com>
2025-08-30 09:39:48 +0530

    Role changes for mysql

---
+++ 20-bastion/bastion.sh
@@ -0,0 +1,15 @@
+# user data to install terraform and increse memory space (Resize EBS Storage)
+#!/bin/bash
+
+# terraform installation
+yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
+# Resize EBS Storage
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/homeVol
+
+xfs_growfs /
+xfs_growfs /home
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id] # getting the baston security group id from locals
   subnet_id = local.public_subnet_id # getting the public subnet id from locals for bastion, otherwise it will create in default VPC
+  
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("bastion.sh") # user data to install terraform and increasing the memory space
+  iam_instance_profile = "TerraformAdmin" #Role configuration for using aws configure
 
   tags = merge(
     local.common_tags,
