386eb894e1188e98dcd5af50960977b41bfd8bce
sivakumar <gsivadevops@gmail.com>
2025-08-30 19:12:50 +0530

    cloudfront changes for domain

--- 91-cdn/main.tf
+++ 91-cdn/main.tf
@@ -1,34 +1,34 @@
 resource "aws_cloudfront_distribution" "roboshop" {
   # Cloud front creation
   origin {
-    domain_name = "cdn.${var.zone_name}"
+    domain_name = "${var.environment}.${var.zone_name}" # Point to your actual backend domain  - dev.devopslearning.store 
+    origin_id  = "${var.environment}.${var.zone_name}"  # Point to your actual backend domain  - dev.devopslearning.store 
     custom_origin_config  {
         http_port              = 80 // Required to be set but not used
         https_port             = 443
         origin_protocol_policy = "https-only"
         origin_ssl_protocols   = ["TLSv1.2"]
     }
-    origin_id  = "cdn.${var.zone_name}"
   }
   enabled = true
-  aliases = ["cdn.${var.zone_name}"]
+  aliases = ["cdn.${var.zone_name}"] # This is the CloudFront alias (CDN endpoint)
 
-  # Cloudefront Cache behavior
+  # Cloudefront default Cache behavior for all traffic
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "cdn.${var.zone_name}"
+    target_origin_id = "${var.environment}.${var.zone_name}" # must match with origin_id
 
     viewer_protocol_policy = "https-only"
     cache_policy_id  = data.aws_cloudfront_cache_policy.cacheDisable.id
   }
 
-  # Cloudefront Cache behavior
+  # Cloudefront Cache behavior for /media/*
   ordered_cache_behavior {
     path_pattern     = "/media/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "cdn.${var.zone_name}"
+    target_origin_id = "${var.environment}.${var.zone_name}" # must match with origin_id
 
     viewer_protocol_policy = "https-only"
     cache_policy_id  = data.aws_cloudfront_cache_policy.cacheEnable.id
@@ -39,7 +39,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations = ["IN", "US", "CA", "GB", "DE"]
+      locations = ["IN", "US", "CA", "GB", "DE"] # add IN if you want India
     }
   }
 
