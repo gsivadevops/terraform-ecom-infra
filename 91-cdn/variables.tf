68f075946d933368d57b1d45e4eed061d7ee7286
sivakumar <gsivadevops@gmail.com>
2025-08-30 11:32:52 +0530

    cloudfront distribution

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z02979802YJHT5DLYSTHT"
+}
+
+variable "zone_name" {
+    default = "devopslearning.store"
+}
